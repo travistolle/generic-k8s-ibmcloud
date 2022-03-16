7b7ef575c5a33385db1d5d8198d7265ce02b8b64
Daniel Benner <dbenner@us.ibm.com>
2022-03-15 22:24:14 -0500

    added new files

--- install-master-via-git.sh
+++ install-master-via-git.sh
@@ -1,10 +1,8 @@
 #!/bin/bash 
 ## IPs of the worker nodes (passed via API call in postman run sequence)
-echo {{node_1_internal_address}} > /root/nodes
-echo {{node_2_internal_address}} >> /root/nodes
-echo {{node_3_internal_address}} >> /root/nodes
-export NODE_LIST='/tmp/nodes'
-export MASTER_PUBLIC_ADDRESS={{master_public_address}}
+echo {{node_1_internal_address}} > /tmp/nodes
+echo {{node_2_internal_address}} >> /tmp/nodes
+echo {{node_3_internal_address}} >> /tmp/nodes
 ## Install Git and clone scripts for control node
 dnf install -y Git
 wget https://raw.githubusercontent.com/travistolle/generic-k8s-ibmcloud/main/install-k8s-master-rhel8.sh -O /tmp/master.sh
