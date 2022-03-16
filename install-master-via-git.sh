f152884849648bd250db60f5bdb59db5ad7640d5
Daniel Benner <dbenner@us.ibm.com>
2022-03-16 00:09:42 -0500

    removed sudo

--- install-master-via-git.sh
+++ install-master-via-git.sh
@@ -4,8 +4,8 @@ echo {{node_1_internal_address}} > /run/media/nodes
 echo {{node_2_internal_address}} >> /run/media/nodes
 echo {{node_3_internal_address}} >> /run/media/nodes
 ## Install Git and clone scripts for control-plane node
-sudo dnf install -y git
-sudo dnf install -y wget
-sudo wget https://github.com/travistolle/generic-k8s-ibmcloud/blob/main/kubeadm-install-flow.sh -O /run/media/master.sh
-sudo chmod +x /run/media/master.sh
-sudo /run/media/master.sh
+dnf install -y git
+dnf install -y wget
+wget https://github.com/travistolle/generic-k8s-ibmcloud/blob/main/kubeadm-install-flow.sh -O /run/media/master.sh
+chmod +x /run/media/master.sh
+/run/media/master.sh
