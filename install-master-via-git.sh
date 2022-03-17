99fb0b6cf0cca17241468c201af6f955d9dafb08
Daniel Benner <dbenner@us.ibm.com>
2022-03-16 21:31:29 -0500

    cloud-init experiments

--- install-master-via-git.sh
+++ install-master-via-git.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 ## IPs of the worker nodes (passed via API call in postman run sequence)
-echo {{node_1_internal_address}} > /run/media/nodes
-echo {{node_2_internal_address}} >> /run/media/nodes
-echo {{node_3_internal_address}} >> /run/media/nodes
+echo {{node_1_internal_address}} > /root/nodes
+echo {{node_2_internal_address}} >> /root/nodes
+echo {{node_3_internal_address}} >> /root/nodes
 ## Install Git and clone scripts for control-plane node
-dnf install -y git
-dnf install -y wget
-wget https://github.com/travistolle/generic-k8s-ibmcloud/blob/main/kubeadm-install-flow.sh -O /run/media/master.sh
-chmod +x /run/media/master.sh
-/run/media/master.sh
+yum install -y git
+yum install -y wget
+mkdir master
+cd master
+git clone https://github.com/travistolle/generic-k8s-ibmcloud.git
+cd generic-k8s-ibmcloud
+chmod +x /root/master/generic-k8s-ibmcloud/master.sh
+/root/master/generic-k8s-ibmcloud/master.sh
