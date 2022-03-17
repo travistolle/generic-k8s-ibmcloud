5a17ca76e29f697b3756516804402c432f77d129
Daniel Benner <dbenner@us.ibm.com>
2022-03-16 21:31:49 -0500

    my cloud-init inital commit

--- install-node-via-git.sh
+++ install-node-via-git.sh
@@ -1,6 +1,9 @@
+#!/bin/bash 
+### Install Git and clone scripts for control node
 dnf install -y git
 dnf install -y wget
-wget https://github.com/travistolle/generic-k8s-ibmcloud/blob/main/kubelet-install-flow.sh -O /run/media/node.sh
-chmod +x /run/media/node.sh
-/run/media/node.sh
+wget https://github.com/travistolle/generic-k8s-ibmcloud/blob/main/kubelet-install-flow.sh -O /root/node.sh
+chmod +x /root/node.sh
+/root/node.sh
+### Set root password so master can use sshpass to send the join cluster command
 echo root:glisten-earshot-harbinger-frank3l | chpasswd
